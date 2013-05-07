486fff246420d960c34f92e91c81980db9ea723f
Iain E. Davis <iain@ruhlendavis.org>
2013-05-07 11:38:01 -0500

    Sketched in main class.

--- src/org/ruhlendavis/mc/anchoredportals/AnchoredPortals.java
+++ src/org/ruhlendavis/mc/anchoredportals/AnchoredPortals.java
@@ -1,14 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.ruhlendavis.mc.anchoredportals;
+
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author Iain E. Davis <iain@ruhlendavis.org>
  */
-public class AnchoredPortals
+public final class AnchoredPortals extends JavaPlugin
 {
+	/**
+	 * Called by Minecraft when enabling the plugin.
+	 */
+	@Override
+	public void onEnable()
+	{
+		
+	}
 
+	/**
+	 * Called by Minecraft when disabling the plugin.
+	 */
+	@Override
+	public void onDisable()
+	{
+
+	}
 }
