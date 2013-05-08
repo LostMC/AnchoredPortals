589c5548af237d42e5c3ecef2292a1b748bc5b0f
Iain E. Davis <iain@ruhlendavis.org>
2013-05-08 13:36:33 -0500

    Stubbed in the events I will probably want to capture.

--- src/org/ruhlendavis/mc/anchoredportals/PlayerListener.java
+++ src/org/ruhlendavis/mc/anchoredportals/PlayerListener.java
@@ -4,7 +4,11 @@
  */
 package org.ruhlendavis.mc.anchoredportals;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 /**
  *
@@ -12,5 +16,18 @@ import org.bukkit.event.Listener;
  */
 public class PlayerListener implements Listener
 {
+	@EventHandler
+	public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent event)
+	{
+	}
 
+  @EventHandler
+  public void onPlayerRespawn(PlayerRespawnEvent event)
+	{
+	}
+	
+	@EventHandler
+	public void onPlayerPortalEvent(PlayerPortalEvent event)
+	{
+	}
 }
