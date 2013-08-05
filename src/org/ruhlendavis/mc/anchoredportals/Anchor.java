a9a4635e538143b25c0322344c77a249567d839a
Iain E. Davis <iain@ruhlendavis.org>
2013-08-05 14:56:48 -0500

    Adding Anchor class

--- src/org/ruhlendavis/mc/anchoredportals/Anchor.java
+++ src/org/ruhlendavis/mc/anchoredportals/Anchor.java
@@ -1,10 +1,45 @@
 package org.ruhlendavis.mc.anchoredportals;
 
+import org.bukkit.Location;
+
 /**
- *
- * @author Iain E. Davis <iain@ruhlendavis.org>
+ * Representation of an "anchor" (link between a given pair of nether portals) in memory.
+ * 
+ * @author Feaelin (Iain E. Davis) <iain@ruhlendavis.org>
  */
 public class Anchor
 {
+	private Location overworldTerminus;
+	private Location netherTerminus;
+	private String playerName;
 
+	public Location getOverworldTerminus()
+	{
+		return overworldTerminus;
+	}
+
+	public void setOverworldTerminus(Location overworldTerminus)
+	{
+		this.overworldTerminus = overworldTerminus;
+	}
+
+	public Location getNetherTerminus()
+	{
+		return netherTerminus;
+	}
+
+	public void setNetherTerminus(Location netherTerminus)
+	{
+		this.netherTerminus = netherTerminus;
+	}
+
+	public String getPlayerName()
+	{
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName)
+	{
+		this.playerName = playerName;
+	}
 }
